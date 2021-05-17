acd620b127de04b66ad500327b189360d0ace6ce
Bijay Tamang <lazon11@outlook.com>
2021-05-17 13:37:10 -0400

    added hooks and context

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,30 +1,64 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
 import { ThemeContext } from "./../contexts/ThemeContext";
-class TodoList extends Component {
-  static contextType = ThemeContext;
-  state = {};
-  render() {
-    const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
+import { TodoListContext } from "./../contexts/TodoListContext";
 
-    return (
-      <div
-        style={{
-          background: theme.background,
-          color: theme.text,
-          height: "140px",
-          textAlign: "center",
-        }}
-      >
-        <p className="item">Plan the family trip</p>
-        <p className="item">Go shopping </p>
-        <p className="item">Go for a walk</p>
-        <button className="ui button primary" onClick={changeTheme}>
-          Change the Theme
-        </button>
-      </div>
-    );
-  }
-}
+const TodoList = () => {
+  const [todo, setTodo] = useState("");
+  const { todos, addTodo, removeTodo } = useContext(TodoListContext);
+  const { isDarkTheme, darkTheme, lightTheme, changeTheme } =
+    useContext(ThemeContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addTodo(todo);
+    setTodo("");
+  };
+  const handleRemoveTodo = (e) => {
+    const id = e.target.id;
+    removeTodo(id);
+  };
+  return (
+    <div
+      style={{
+        background: theme.background,
+        color: theme.text,
+        textAlign: "center",
+      }}
+    >
+      {todos.length ? (
+        todos.map((todo) => {
+          return (
+            <p
+              key={todo.id}
+              id={todo.id}
+              onClick={handleRemoveTodo}
+              className="item"
+            >
+              {todo.text}
+            </p>
+          );
+        })
+      ) : (
+        <div>You have no todos.</div>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="">Add Todo: </label>
+        <input type="text" id="todo" value={todo} onChange={handleChange} />
+        <input
+          type="submit"
+          value="Add New Todo"
+          className="ui button primary"
+        />
+      </form>
+
+      <button className="ui button primary" onClick={changeTheme}>
+        Change the Theme
+      </button>
+    </div>
+  );
+};
 
 export default TodoList;
